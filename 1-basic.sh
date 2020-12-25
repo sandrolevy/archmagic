b5800300f4e7b3d970e34289775f70c09aee4f1b
Sandro Levy <sandrolevyjr@gmail.com>
2020-12-25 17:18:00 -0300

    Create 1-basic.sh

---
+++ 1-basic.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+echo -e "\nInstalling Base System\n"
+
+PKGS=(
+
+    # --- XORG Display Rendering
+        'xorg'                  # Base Package
+        'xorg-drivers'          # Display Drivers 
+        'termite'               # Terminal for TTY
+        'xorg-server'           # XOrg server
+        'xorg-apps'             # XOrg apps group
+        'xorg-xinit'            # XOrg init
+        'xorg-xinput'           # Xorg xinput
+        'mesa'                  # Open source version of OpenGL
+
+    # --- Setup Desktop
+        'awesome'               # Awesome Desktop
+        'xfce4-power-manager'   # Power Manager 
+        'dmenu'                 # Menu System
+        'picom'                 # Translucent Windows
+        'xclip'                 # System Clipboard
+        'gnome-polkit'          # Elevate Applications
+        'lxappearance'          # Set System Themes
+
+    # --- Login Display Manager
+        'ly'                    # Base Login Manager
+
+    # --- Networking Setup
+        'wpa_supplicant'            # Key negotiation for WPA wireless networks
+        'dialog'                    # Enables shell scripts to trigger dialog box
+        'network-manager-applet'    # System tray icon/utility for network connectivity
+        'libsecret'                 # Library for storing passwords
+    
+    # --- Audio
+        'alsa-utils'        # Advanced Linux Sound Architecture (ALSA) Components https://alsa.opensrc.org/
+        'alsa-plugins'      # ALSA plugins
+        'pulseaudio'        # Pulse Audio sound components
+        'pulseaudio-alsa'   # ALSA configuration for pulse audio
+        'pavucontrol'       # Pulse Audio volume control
+        'pnmixer'           # System tray volume control
+
+    # --- Bluetooth
+        'bluez'                 # Daemons for the bluetooth protocol stack
+        'bluez-utils'           # Bluetooth development and debugging utilities
+        'bluez-firmware'        # Firmwares for Broadcom BCM203x and STLC2300 Bluetooth chips
+        'blueberry'             # Bluetooth configuration tool
+        'pulseaudio-bluetooth'  # Bluetooth support for PulseAudio
+    
+    # --- Printers
+        'cups'                  # Open source printer drivers
+        'cups-pdf'              # PDF support for cups
+        'ghostscript'           # PostScript interpreter
+        'gsfonts'               # Adobe Postscript replacement fonts
+        'hplip'                 # HP Drivers
+        'system-config-printer' # Printer setup  utility
+)
+
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING: ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+echo -e "\nDone!\n"
