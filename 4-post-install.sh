ce5b5213dfc90367a570d676447482ed5b96cab1
Sandro Levy <sandrolevyjr@gmail.com>
2020-12-26 13:24:53 -0300

    Update 4-post-install.sh

--- 4-post-install.sh
+++ 4-post-install.sh
@@ -4,18 +4,15 @@ echo -e "\nFINAL SETUP AND CONFIGURATION"
 
 # ------------------------------------------------------------------------
 
-# Disable all Power Saving Stuff
-xset -dpms
-xset s off
-# ------------------------------------------------------------------------
-
 echo -e "\nEnabling Login Display Manager"
 
 sudo systemctl enable ly.service
 
 # -----------------------------------------------------------------------
-
+echo -e "\n Enabling Touchpad Settings"
 sudo cp ./30-touchpad.conf /etc/X11/xorg.conf.d/
+
+echo -e "\n Unmutting Audio"
 
 amixer sset Master unmute
 amixer sset Speaker unmute
